d2667442dcca165f30d3a931dac56cb833972a36
Gautam Dheer <gautam.litesh@gmail.com>
2024-09-24 16:22:10 +0530

    remove landing page

---
+++ anna247/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html", ],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        robocondo: ['Roboto Condensed', 'sans-serif'],
+       },
+    },
+  },
+  plugins: [],
+}
+
